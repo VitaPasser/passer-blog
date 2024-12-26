b40332ff3baafcefaee08b63b4a1a244a6666c8d
VitaPasser <vitapasser@gmail.com>
2024-12-26 17:21:07 +0200

    Pagination fixed style

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -57,7 +57,7 @@ const Pagination = ({
                             href={"/posts?page=" + page}
                             className={`flex items-center justify-center aspect-square w-[40px] text-sm font-medium rounded-md ${page === currentPage
                                 ? 'bg-purple-50 text-purple-700 dark:bg-slate-50 dark:text-slate-700'
-                                : 'bg-white dark:bg-haiti text-pale-sky dark:text-ghost dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700'
+                                : 'bg-white dark:bg-haiti text-pale-sky dark:text-ghost hover:bg-gray-100 dark:hover:bg-gray-700'
                                 }`}
                         >
                             {page}
