fb1552df0064ad43800513404a5271a67d35cc72
VitaPasser <vitapasser@gmail.com>
2025-01-02 11:19:58 +0200

    Replace title in navbar to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <>
+    <header className='flex justify-between items-center p-5 xl:pt-[30px] xl:pb-0 xl:px-0'>
+      <Link href='/'>
+        <p className='font-semibold text-lg leading-6'>Passer Blog</p>
+      </Link>
       <Navbar />
-    </>
+    </header>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,10 @@ import React from 'react'
 import Menu from './Menu/Menu';
 import TurnDarkMode from './Menu/TurnDarkMode';
 import MenuLink from './Menu/MenuLink';
-import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-between items-center p-5 xl:pt-[30px] xl:pb-0 xl:px-0'>
-            <Link href='/'>
-                <p className='font-semibold text-lg leading-6'>Vita Passer</p>
-            </Link>
+        <>
             <Menu className='md:hidden' />
             <nav className='hidden md:flex gap-[14px]'>
                 <MenuLink href='/posts'>Blog</MenuLink>
@@ -18,7 +14,7 @@ const Navbar = () => {
                 <MenuLink href='/newsletter'>Newsletter</MenuLink>
                 <TurnDarkMode />
             </nav>
-        </nav>
+        </>
     )
 }
 
